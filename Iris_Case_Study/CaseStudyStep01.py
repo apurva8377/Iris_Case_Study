56a2277fdf2450a77f1fc620515f80db5ae104c2
apurva8377 <apurvashinde098@gmail.com>
2026-05-26 11:28:40 +0530

    Add files via upload

---
+++ Iris_Case_Study/CaseStudyStep01.py
@@ -0,0 +1,42 @@
+##########################################################
+#
+#  Case Study Name : Iris_Case_Study
+#  Description     : Load Dataset – Reads iris.csv using Pandas.
+#  Auther          : Apurva Vilas Shinde
+#  date            : 26/05/2026
+#
+##########################################################
+
+import pandas as pd
+
+import matplotlib.pylab as plt
+
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+
+from sklearn.metrics import(
+    accuracy_score,
+    confusion_matrix,
+    classification_report,
+    ConfusionMatrixDisplay
+)
+
+Border = "-"*40
+
+##############################################
+# Step 1 : Load the Dadatset
+##############################################
+print(Border)
+print("Step 1 : Load the dataset")
+print(Border)
+
+DatasetPath = "iris.csv"
+
+df = pd.read_csv(DatasetPath)
+
+print("Dataset gets loaded succesfully...")
+print("Initial entries from dataset : ")
+print(df.head())
---
+++ Iris_Case_Study/CaseStudyStep03.py
@@ -0,0 +1,88 @@
+##########################################################
+#
+#  Case Study Name : Iris_Case_Study
+#  Description     : Select Variables –
+#                       X (Independent variables): sepal length, sepal width, petal length, petal width
+#                       Y (Dependent variable): flower variety
+#  Auther          : Apurva Vilas Shinde
+#  date            : 26/05/2026
+#
+##########################################################
+
+import pandas as pd
+
+import matplotlib.pylab as plt
+
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+
+from sklearn.metrics import(
+    accuracy_score,
+    confusion_matrix,
+    classification_report,
+    ConfusionMatrixDisplay
+)
+
+Border = "-"*40
+
+##############################################
+# Step 1 : Load the Dadatset
+##############################################
+print(Border)
+print("Step 1 : Load the dataset")
+print(Border)
+
+DatasetPath = "iris.csv"
+
+df = pd.read_csv(DatasetPath)
+
+print("Dataset gets loaded succesfully...")
+print("Initial entries from dataset : ")
+print(df.head())
+
+###########################################################
+# Step 2 : Data Analysis (EDA - Exploratory Data Analysis)
+###########################################################
+print(Border)
+print("Step 2 : Data Analysis")
+print(Border)
+
+print("Shape of dataset : ",df.shape)
+
+print("Column Names : ",list(df.columns))
+
+print("Missing values (Per Column) :")
+print(df.isnull().sum())
+
+print("Class Distribution (Species count) : ")
+print(df["variety"].value_counts())
+
+print("Statistical Report of dataset :")
+print(df.describe())
+
+###########################################################
+# Step 3 : Decide Independent & Dependent variable
+###########################################################
+print(Border)
+print("Step 3 : Decide Independent & Dependent variable")
+print(Border)
+
+# X : Independent variable
+# Y : Dependent variable
+
+feature_cols = [
+    "sepal.length",
+    "sepal.width",
+    "petal.length",
+    "petal.width"
+]
+
+X = df[feature_cols]
+Y = df["variety"]
+
+print("X shape : ",X.shape)
+print("Y shape : ",Y.shape)
+
